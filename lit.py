779e10f7353dec85a03dd1afecc3b82b7bf78cc0
priyanshup1 <72408237+priyanshup1@users.noreply.github.com>
2021-06-14 13:06:54 +0530

    Add files via upload

---
+++ lit.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+import pyqrcode
+import png
+st.title("welcome to stream it")
+st.title("GET YOUR QR_CODE")
+urlink=st.text_input("enter the url",)
+url = pyqrcode.create(urlink)
+url.svg('uca-url.svg', scale=3)
+url.eps('uca-url.eps', scale=2)
+url.terminal(quiet_zone=1)
+#print(url.terminal(quiet_zone=1))
+url.png("myqrcode.png",scale=6)
+st.image("/Users/priya/Desktop/workspace/github/qrcode/myqrcode.png")
